3093518b40ab616c5e255ac4647072e6927e2ae5
LuiAr <louis.arbey@efrei.net>
2024-10-09 16:16:25 +0200

    first version of the app

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+import os
 
-app = Flask(__name__)
+template_dir = os.path.abspath('app/templates')
+app = Flask(__name__, template_folder=template_dir)
 
 from app import routes
